11d720e97a3820893b0c1d8feaeae072dc652f39
wilfredkisku <wilfred.kisku@gmail.com>
2021-03-01 01:49:08 +0530

    changes in utils

--- flownetS.py
+++ flownetS.py
@@ -122,16 +122,13 @@ class flownetS:
         plt.xlabel('epoch')
         plt.legend(['train'], loc='upper left')
         plt.show()
-        return None
 		
     def makePrediction(img1,img2):
-        model = load_model('./models/flowNetSimple_1000.h5')
-        img_stack = np.zeros((1, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS*2), dtype = np.uint8)
+        model = load_model('./models/flowNetSimple_500.h5')
+        img_stack = np.zeros((1, img1.shape[0], img1.shape[1], IMG_CHANNELS*2), dtype = np.uint8)
         img_stack[0] = np.concatenate((img1, img2), axis = 2)
         pred_test = model.predict(img_stack, verbose = 1)
 
-        print(pred_test[0].shape)
-        print(pred_test[0])
 		
 if __name__ == '__main__':
     
@@ -150,20 +147,21 @@ if __name__ == '__main__':
     for idx, key in enumerate(x_files.keys()):
         cnt_n += len(x_files[key])
 
-    X_train = np.zeros(((cnt_n-(len(x_files.keys())*2))*3, IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS*2), dtype=np.uint8)
-    Y_train = np.zeros(((cnt_n-(len(x_files.keys())*2))*3, IMG_HEIGHT, IMG_WIDTH, FLO_CHANNELS), dtype=np.float32)
+
+    X_train = np.zeros(((cnt_n-(len(x_files.keys())*2))*3, objpre.IMG_HEIGHT, objpre.IMG_WIDTH, objpre.IMG_CHANNELS*2), dtype=np.uint8)
+    Y_train = np.zeros(((cnt_n-(len(x_files.keys())*2))*3, objpre.IMG_HEIGHT, objpre.IMG_WIDTH, objpre.FLO_CHANNELS), dtype=np.float32)
 
     obj.createDataset(X_train, Y_train, x, y, x_files, y_files)
 
+    #checkpointer = ModelCheckpoint('./models/flownetS-weights-500.ckpt', verbose=1, save_weights_only=True, save_best_only=True)
 
-    checkpointer = ModelCheckpoint('/workspace/storage/flownet-tf/models/flowNetSimple_2000.h5', verbose=1, save_best_only=True)
-    results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=32, shuffle=True, epochs=2000, callbacks=[checkpointer])
+    checkpointer = ModelCheckpoint('./models/flowNetS-complete-500.h5', verbose=1, save_best_only=True)
+    results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=32, shuffle=True, epochs=500, callbacks=[checkpointer])
 
     hist_df = pd.DataFrame(results.history)
-    hist_csv_file = '/workspace/storage/flownet-tf/models/history.csv'
+    hist_csv_file = './models/flownetS-history-500.csv'
     
     with open(hist_csv_file, mode = 'w') as f:
         hist_df.to_csv(f)
     
     print('End of Training!')
-
--- tests/flownet-model-test.py
+++
@@ -1,9 +0,0 @@
-'''
-This program is free software: you can use, modify and/or redistribute it
-under the terms of the simplified BSD License. You should have received a
-copy of this license along this program.
-
-Copyright 2020, wilfred kisku <kisku.1@iitj.ac.in>
-All rights reserved.
-'''
-
